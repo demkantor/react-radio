7b2977e711b3a98ce6cd36fb78be13a87d7b94d3
demkantor <demkantor@gmail.com>
2020-05-14 23:40:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AudioPlayer from 'react-modular-audio-player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  rearrangedPlayer = [
+    {
+      className: "tier-top",
+      style: {margin: "0.8rem"},
+      innerComponents: [
+        { 
+          type: "play",
+          style: {width: "fit-content"}
+        },
+        {
+          type: "rewind",
+          style: {width: "fit-content"}
+        },
+        {
+          type: "forward",
+          style: {width: "fit-content"}
+        },
+        {
+          type: "volume"
+        }
+      ]
+    },
+    {
+      className: "tier-bottom",
+      style: {margin: "0rem 0.3rem 0.3rem 0.3rem"},
+      innerComponents: [
+        {
+          type: "time",
+          style: {width: "fit-content"}
+        },
+        {
+          type: "seek"
+        }
+      ]
+    }
+  ]
+
+
+  playlist = [
+    { src: "http://167.114.64.181:8153/stream?type=http&amp;nocache=14875",
+      title: "Song",
+      artist: "Singer" },
+    { src: "/moreMusic.mp3",
+      title: "Another Song",
+      artist: "Another Singer" }
+  ];
+
+  render() {
+    return (
+      <div className="app">
+        <div class="station">
+          <AudioPlayer 
+          rearrange={this.rearrangedPlayer}
+            audioFiles={this.playlist}
+            playerWidth="50em"
+            fontWeight="900"
+          />
+          <div class="playing">
+               <div class="rect1"></div>
+               <div class="rect2"></div>
+               <div class="rect3"></div>
+               <div class="rect4"></div>
+               <div class="rect5"></div>
+          </div>
+        </div>
+        <div id="station0" class="station">
+          <AudioPlayer 
+          rearrange={this.rearrangedPlayer}
+            audioFiles={this.playlist}
+            playerWidth="50em"
+            fontWeight="900"
+          />
+        </div>
+        <div id="station0" class="station">
+          <AudioPlayer 
+          rearrange={this.rearrangedPlayer}
+            audioFiles={this.playlist}
+            playerWidth="50em"
+            fontWeight="900"
+          />
+        </div>
+    
+         
+      </div>
+    )
+  }
 }
 
 export default App;
