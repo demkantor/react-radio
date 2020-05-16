f4a150321c815a8de6797179d30672b2a7a5ad0b
demkantor <demkantor@gmail.com>
2020-05-15 21:07:11 -0500

    removed mongo for now

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
@@ -10,11 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/internet_raio`, {useUnifiedTopology: true, useNewUrlParser: true });
-  const connection = mongoose.connection;
-  connection.once('open', function() {
-    console.log("MongoDB database connected...");
-})
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/internet_raio`, {useUnifiedTopology: true, useNewUrlParser: true });
+//   const connection = mongoose.connection;
+//   connection.once('open', function() {
+//     console.log("MongoDB database connected...");
+// })
 
 /** ---------- START SERVER ---------- **/
 /** Listen * */
