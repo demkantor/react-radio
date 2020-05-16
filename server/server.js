2bfe9e357bd3d03f65b4cda648fc7fbcf5ab2810
demkantor <demkantor@gmail.com>
2020-05-15 21:15:46 -0500

    removed monogo

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/internet_raio`, {useUnifiedTopology: true, useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/internet_radio`, {useUnifiedTopology: true, useNewUrlParser: true });
 //   const connection = mongoose.connection;
 //   connection.once('open', function() {
 //     console.log("MongoDB database connected...");
